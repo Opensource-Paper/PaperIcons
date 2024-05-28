54e3d19beec9a2633dcdc05086658cfee3cd6520
Brick_Wall <118539051+GreenestGoat@users.noreply.github.com>
2024-05-28 15:28:36 +0100

    Update import-dev.js

--- cdn/import-dev.js
+++ cdn/import-dev.js
@@ -12,9 +12,10 @@ class PaperIcons {
 <circle  cx="12" cy="12" r="10" stroke="currentColor" stroke-width="2"/>
 <circle cx="12" cy="12" r="2" stroke="currentColor" stroke-width="2"/>
 </svg>`,
-      "more-horizontal": `<svg color="currentColor" width="24" height="24" stroke="currentColor" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle  cx="12" cy="12" r="10" stroke="currentColor" stroke-width="2"/>
-<circle cx="12" cy="12" r="2" stroke="currentColor" stroke-width="2"/>
+      "more-horizontal": `<svg width="24" height="24" color="currentColor" stroke="currentColor" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="12" cy="12" r="1" stroke="currentColor" stroke-width="2"/>
+<circle cx="5" cy="12" r="1" stroke="currentColor" stroke-width="2"/>
+<circle cx="19" cy="12" r="1" stroke="currentColor" stroke-width="2"/>
 </svg>`,
       // Add more icons here
     };
